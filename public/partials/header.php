90a21f362f0e3be50a5d6b5d99b5b83d35272577
TimQwees <artemnersisyan777@gmail.com>
2026-07-10 18:35:52 +0300

    update

--- public/partials/header.php
+++ public/partials/header.php
@@ -44,33 +44,20 @@ $siteUrl = "$scheme://$host";
 
   <?php
   $gscId = $_ENV["GSC_ID"] ?? "";
-  $ywId = $_ENV["YW_ID"] ?? "";
   $ga4Id = $_ENV["GA4_ID"] ?? "";
   $ymId = $_ENV["YM_ID"] ?? "";
   $ywebId = $_ENV["YWEB_ID"] ?? "";
   ?>
-  <?php if (
-    $gscId
-  ): ?><meta name="google-site-verification" content="<?= $gscId ?>"><?php endif; ?>
-  <?php if (
-    $ywId
-  ): ?><meta name="yandex-verification" content="<?= $ywId ?>"><?php endif; ?>
   <meta name="geo.region" content="RU-MOW">
   <meta name="geo.placename" content="Москва">
   <meta name="geo.position" content="55.765833;37.618889">
   <meta name="ICBM" content="55.765833, 37.618889">
     
   <!-- SEO METRIKS -->
+  <?php if ($gscId): ?><meta name="google-site-verification" content="<?= htmlspecialchars($gscId) ?>"><?php endif; ?>
   <?php if ($ywebId): ?><meta name="yandex-verification" content="<?= htmlspecialchars($ywebId) ?>" /><?php endif; ?>
-  <?php if ($ga4Id): ?>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=<?= htmlspecialchars($ga4Id) ?>"></script>
-  <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','<?= htmlspecialchars($ga4Id) ?>');</script>
-  <?php endif; ?>
-
-  <?php if ($ymId): ?>
-  <script>(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');ym(<?= htmlspecialchars($ymId) ?>,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});</script>
-  <noscript><div><img src="https://mc.yandex.ru/watch/<?= htmlspecialchars($ymId) ?>" style="position:absolute;left:-9999px" alt=""></div></noscript>
-  <?php endif; ?>
+  <?php if ($ga4Id): ?><script async src="https://www.googletagmanager.com/gtag/js?id=<?= htmlspecialchars($ga4Id) ?>"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','<?= htmlspecialchars($ga4Id) ?>');</script><?php endif; ?>
+  <?php if ($ymId): ?><script>(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r)return;}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');ym(<?= htmlspecialchars($ymId) ?>,'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});</script><noscript><div><img src="https://mc.yandex.ru/watch/<?= htmlspecialchars($ymId) ?>" style="position:absolute;left:-9999px" alt=""></div></noscript><?php endif; ?>
 </head>
 <body itemscope itemtype="https://schema.org/LocalBusiness">
 
